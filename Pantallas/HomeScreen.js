3c654d705c3da38789b3c0a566f2bec144978430
diegovinals98 <diego.vinalslage@gmail.com>
2024-02-22 13:27:53 +0100

    comentarios y diseño basico

--- Pantallas/HomeScreen.js
+++ Pantallas/HomeScreen.js
@@ -1,116 +1,98 @@
-import { View,
-  Text,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  button,
-  Dimensions,
-  Modal,
-  FlatList
-} from 'react-native';
-
-// Importaciones de React y elementos de React Native.
+// Importaciones de React, React Native y otras librerías.
 import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions, Modal } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { globalStyles } from '../estilosGlobales.js';
-const windowHeigh = Dimensions.get('window').height;
-import { useUser } from '../userContext.js'; // Importa el hook useUser
+import { useUser } from '../userContext.js'; // Importa el contexto del usuario.
 import { StatusBar } from 'expo-status-bar';
+import { globalStyles } from '../estilosGlobales.js'; // Importa estilos globales.
 
+// Obtiene las dimensiones de la ventana del dispositivo.
+const windowHeigh = Dimensions.get('window').height;
 
-
-
+// Componente principal de la pantalla de inicio.
 const HomeScreen = () => {
-  
-
+  // Hook de navegación y rutas de react-navigation.
   const route = useRoute();
   const navigation = useNavigation();
-  const { user } = useUser(); // Accede a los datos del usuario y la función para actualizarlos
+
+  // Accede a los datos del usuario desde el contexto.
+  const { user } = useUser();
+  // Calcula las iniciales del usuario para mostrar.
   const iniciales = user?.nombre ? `${user?.nombre.charAt(0)}${user?.apellidos.charAt(0)}` : '';
+
   // Estados del componente.
-  // data: Almacena datos de usuarios.
-  const [data, setData] = useState([]);
-  // seriesData: Almacena datos generales de las series.
-  const [seriesData, setSeriesData] = useState([]);
-  // serieDetalle: Almacena detalles específicos de una serie seleccionada.
-  const [serieDetalle, setSerieDetalle] = useState([]);
+  const [data, setData] = useState([]); // Estado para datos de usuarios.
+  const [seriesData, setSeriesData] = useState([]); // Estado para datos generales de series.
+  const [serieDetalle, setSerieDetalle] = useState([]); // Estado para detalles específicos de una serie.
 
-
-  // datos del usuario
-  
-
-  // Para el dropwdown menu de los grupos
+  // Estado para la visibilidad del menú desplegable y el grupo seleccionado.
   const [isVisible, setIsVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
-  const [TodosGrupos, setTodosGrupos] = useState([]);
+  const [TodosGrupos, setTodosGrupos] = useState([]); // Estado para almacenar todos los grupos.
 
-
+  // Función para manejar la selección de un grupo.
   const handleSelectItem = (item) => {
     setSelectedItem(item.Nombre_grupo);
     setIsVisible(false);
   };
 
-  const groups = ['Grupo 1', 'Grupo 2', 'Grupo 3']; // Añade aquí más grupos si es necesario
-
+  // Función para realizar la llamada a la API y obtener los grupos del Usuario
   function llamarAGrupos(){
-    setIsVisible(true)
-    // Llamada a la API para obtener datos de la tabla Grupos del usuario
+    setIsVisible(true);
     fetch(`http://10.0.0.36:3000/grupos/${user?.id}`)
-    .then((response) => response.json())
-    .then((json) => setTodosGrupos(json))
-    .catch((error) => console.error('Error al obtener los grupos:', error));
+      .then((response) => response.json())
+      .then((json) => setTodosGrupos(json))
+      .catch((error) => console.error('Error al obtener los grupos:', error));
     console.log(TodosGrupos);
   }
-  
-  // useEffect se ejecuta después de la renderización del componente.
+
+  // Efecto para cargar datos de una serie específica.
   useEffect(() => {
-  
-    // Llamada a la API de TMDb para obtener información de una serie específica.
-    const serie_a_buscar = 'Masters of the air';
+    // URL de la API de TMDb para buscar series.
+    const serie_a_buscar = 'reacher';
     let apiSeries = `https://api.themoviedb.org/3/search/tv?api_key=c51082efa7d62553e4c05812ebf6040e&language=es-ES&page=1&query=${serie_a_buscar}&include_adult=false`;
-  
+
+    // Llamada a la API para obtener los detalles de la serie.
     fetch(apiSeries)
       .then(response => response.json())
       .then(data => {
         const serieId = data.results[0].id;
-
-        // Obteniendo los detalles completos de la serie usando su ID.
         const apiSerieDetalle = `https://api.themoviedb.org/3/tv/${serieId}?api_key=c51082efa7d62553e4c05812ebf6040e&language=es-ES`;
         return fetch(apiSerieDetalle);
       })
       .then(response => response.json())
       .then(serieDetalle => {
-        // Almacenando los detalles de la serie en el estado serieDetalle.
         setSerieDetalle(serieDetalle);
       })
       .catch(error => console.error('Error:', error));
-  }, []); // El array vacío asegura que useEffect se ejecute solo una vez después del montaje inicial.
+  }, []); // El array vacío como segundo argumento asegura que useEffect se ejecute solo una vez.
 
-
+  // Función para navegar a la pantalla de ajustes.
   const handleSettings = () => {
     navigation.navigate('Settings');
   }
-  
+
+  // Renderizado del componente.
   return (
-    
     <View style={[globalStyles.container, styles.container]}>
       <StatusBar/>
 
+      {/* Renderizado de la fila superior con las iniciales del usuario y el botón de grupos. */}
       <View style={styles.row}>
-    <TouchableOpacity style={styles.circle} onPress={() => handleSettings()}>
-      <Text style={styles.initials}>{iniciales}</Text>
-    </TouchableOpacity>
+        <TouchableOpacity style={styles.circle} onPress={() => handleSettings()}>
+          <Text style={styles.initials}>{iniciales}</Text>
+        </TouchableOpacity>
 
-    <TouchableOpacity
-        style={styles.buttonGroup}
-        onPress={() => llamarAGrupos()}
-      >
-        <Text style={styles.buttonText}>{selectedItem || 'Select an Item'}</Text>
-        <Text style={styles.dropdownIcon}>▼</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity
+          style={styles.buttonGroup}
+          onPress={() => llamarAGrupos()}
+        >
+          <Text style={styles.buttonText}>{selectedItem || 'Select an Item'}</Text>
+          <Text style={styles.dropdownIcon}>▼</Text>
+        </TouchableOpacity>
+      </View>
 
-
+      {/* Modal para mostrar los grupos disponibles. */}
       <Modal
         transparent={true}
         visible={isVisible}
@@ -121,51 +103,44 @@ const HomeScreen = () => {
           activeOpacity={1}
           onPressOut={() => setIsVisible(false)}
         >
-                <View style={styles.modalContent}>
-          {TodosGrupos.map((group, index) => (
-            <TouchableOpacity
-              key={index}
-              style={styles.item}
-              onPress={() => handleSelectItem(group)}
-            >
-              <Text style={styles.itemText}>{group.Nombre_grupo}</Text>
+          <View style={styles.modalContent}>
+            {TodosGrupos.map((group, index) => (
+              <TouchableOpacity
+                key={index}
+                style={styles.item}
+                onPress={() => handleSelectItem(group)}
+              >
+                <Text style={styles.itemText}>{group.Nombre_grupo}</Text>
+              </TouchableOpacity>
+            ))}
+
+            {/* Botón para añadir un nuevo grupo. */}
+            <TouchableOpacity style={styles.item}>
+              <Text style={styles.itemText}>+ Añadir Grupo</Text>
             </TouchableOpacity>
-          ))}
-
-          <TouchableOpacity
-            style={styles.item}
-          >
-            <Text style={styles.itemText}>+ Añadir Grupo</Text>
-          </TouchableOpacity>
-</View>
-
-
-
-
-
-        
-        
+          </View>
         </TouchableOpacity>
       </Modal>
+
       
-      <Text>Bienvenido, {user?.nombre} {user?.apellidos} con id: {user?.id}</Text>
-      
+
+      {/* Renderizado de los detalles de la serie seleccionada. */}
       {serieDetalle.name && (
         <View style={styles.serieDetailContainer}>
-          <Text style={styles.serieTitle}>Titulo: {serieDetalle.name}</Text>
+          <Text style={styles.serieTitle}>{serieDetalle.name}</Text>
           <Text style={styles.serieOverview}>Descripcion: {serieDetalle.overview}</Text>
-          {/* Renderizado de los detalles de cada temporada. */}
           {serieDetalle.seasons && serieDetalle.seasons.map((season) => (
-            <Text key={season.id}>
+            <Text key={season.id} style={styles.serieOverview}>
               Temporada {season.season_number}: {season.episode_count} episodios
             </Text>
           ))}
           <Text style={styles.serieOverview}>Status: {serieDetalle.status}</Text>
         </View>
       )}
+      {/* Mensaje de bienvenida y detalles del usuario. */}
+      <Text>Bienvenido, {user?.nombre} {user?.apellidos} con id: {user?.id}</Text>
       
     </View>
-
   );
 };
 
@@ -237,6 +212,24 @@ const styles = StyleSheet.create({
   },
   itemText: {
     fontSize: 16,
+  },
+  serieDetailContainer:{
+    width:'90%'
+  }, serieTitle: {
+    fontSize: 24, // Tamaño grande para el título
+    fontWeight: 'bold', // Negrita para resaltar
+    color: '#4A90E2', // Un color llamativo pero no demasiado intenso
+    marginBottom: 8, // Espacio debajo del título
+    textAlign: 'center', // Centrar el texto
+  },
+
+  serieOverview: {
+    fontSize: 16, // Tamaño moderado para la descripción
+    color: '#333333', // Color oscuro para fácil lectura
+    lineHeight: 24, // Espacio entre líneas para mejor legibilidad
+    textAlign: 'justify', // Justificar para dar una apariencia de bloque de texto
+    paddingHorizontal: 10, // Espacio horizontal para no pegar al borde
+    marginBottom: 12, // Espacio debajo del párrafo
   },
 });
 
